69204ef09f868be0ee2365712776a2dd3f2c78f2
SanjidHaque <sanjidulhaque@gmail.com>
2019-11-25 22:35:20 +0600

    Setting up the basic route structure.

---
+++ api/routes/orders.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /orders'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /orders'
+    });
+});
+
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+
+    res.status(200).json({
+        message: 'order /GET/{id}',
+        id: id
+    });
+
+});
+
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+
+    res.status(200).json({
+        message: 'order /DELETE/{id}',
+        id: id
+    })
+});
+
+module.exports = router;
---
+++ api/routes/products.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
+    });
+});
+
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    res.status(200).json({
+        id: id
+    });
+
+});
+
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    res.status(200).json({
+        id: id + ' is Patching'
+    });
+
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    res.status(200).json({
+        id: id + ' is Deleting'
+    });
+
+});
+
+module.exports = router;
