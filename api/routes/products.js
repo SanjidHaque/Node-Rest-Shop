9fa56d7d6cf9dd9842cf4c591405f4e68f7428e9
SanjidHaque <sanjidulhaque@gmail.com>
2019-11-26 03:13:35 +0600

    Added mongoose to the project.

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
+// const Product = module.exports.Product = mongoose.model('Product', productSchema);
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -9,18 +12,34 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+
+
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+
     res.status(201).json({
-        message: 'Handling POST requests to /products'
+        message: 'Handling POST requests to /products',
+        createdProduct: product
     });
 });
 
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-
-    res.status(200).json({
-        id: id
-    });
+    Product.findById()
 
 });
 
