df2171733900ca56f40a847b69130a9d8ed37137
SanjidHaque <sanjidulhaque@gmail.com>
2019-11-27 01:20:42 +0600

    Order and product schema, CRUD added in API.

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,33 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /orders'
-    });
+    Order.find()
+        .exec()
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
 
-    res.status(201).json({
-        message: 'Handling POST requests to /orders',
-        order: order
-    });
+    Product.findById(req.body.productId)
+        .then(data => {
+            if (!data) {
+                return res.status(404).json({
+                    message: 'Resource not found'
+                });
+            }
+
+            const order = new Order({
+                _id: new mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId
+            });
+
+            return order.save();
+
+        })
+        .then(data => {
+            res.status(201).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
+
 });
 
 
 router.get('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
+    Order.findById(id)
+        .exec()
+        .then((data) => {
 
-    res.status(200).json({
-        message: 'order /GET/{id}',
-        id: id
-    });
+            if (data) {
+                res.status(200).json(data);
+            } else {
+                res.status(404).json({
+                    message: 'Resource not found'
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
+
 
 });
 
@@ -35,10 +76,16 @@ router.get('/:orderId', (req, res, next) => {
 router.delete('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
 
-    res.status(200).json({
-        message: 'order /DELETE/{id}',
-        id: id
-    })
+    Product.remove({ _id: id })
+        .exec()
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
 });
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
-// const Product = module.exports.Product = mongoose.model('Product', productSchema);
 const Product = require('../models/product');
 
+
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    });
+
+    Product.find()
+        .exec()
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
@@ -19,46 +26,79 @@ router.post('/', (req, res, next) => {
         price: req.body.price
     });
 
-
-    product
-        .save()
+    product.save()
         .then(result => {
-            console.log(result);
+            res.status(201).json({
+                createdProduct: result
+            });
         })
         .catch(error => {
-          console.log(error);
+            res.status(500).json({
+                error: error
+            });
         });
 
-
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
-    });
 });
 
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    Product.findById()
+    Product.findById(id)
+        .exec()
+        .then((data) => {
+
+            if (data) {
+                res.status(200).json(data);
+            } else {
+                res.status(404).json({
+                    message: 'Resource not found'
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
 
 });
 
 
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
+    const updateOps = {};
 
-    res.status(200).json({
-        id: id + ' is Patching'
-    });
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+
+    Product.update({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
 
 });
 
+
+
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
-
-    res.status(200).json({
-        id: id + ' is Deleting'
-    });
+    Product.remove({ _id: id })
+        .exec()
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
 
 });
 
